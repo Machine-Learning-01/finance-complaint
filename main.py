2c584a46fb46bb544bbde344901055426557543b
avnish-yadav <avnish@ineuron.ai>
2022-11-11 12:19:02 +0530

    updated

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import os
+from finance_complaint.constant.environment.variable_key import AWS_ACCESS_KEY_ID_ENV_KEY,AWS_SECRET_ACCESS_KEY_ENV_KEY
+from dotenv import load_dotenv
+load_dotenv()
+access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY, )
+secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY, )
+print(access_key_id,secret_access_key)
 import argparse
 from finance_complaint.exception import FinanceException
 from finance_complaint.pipeline import TrainingPipeline, PredictionPipeline
 from finance_complaint.logger import logger
 from finance_complaint.config.pipeline.training import FinanceConfig
 import sys
+
 
 
 def start_training(start=False):
